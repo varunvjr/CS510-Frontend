c65aeb18e88ffef1fa8849c70a5b4c79ff65b728
varunvjr <varun.vjr@gmail.com>
2021-10-28 16:33:37 -0500

    count caseinsensitive to be handled

--- hw3/03-count.js
+++ hw3/03-count.js
@@ -1,3 +1,35 @@
 // Enter your code here
 
+const word=document.getElementById("userinput");
+word.addEventListener("input",(e)=>{
+    e.preventDefault();
+    const key=word.value.trim();
+    console.log("keyword",key);
+    const ul=key.length;
+    console.log("Length: ",ul);
+    const lk=key.toLowerCase();
+    console.log("lk  ",lk);
+    let paragraph=document.getElementById("intro").innerText;
+    if(ul>=1){
+    let re = new RegExp(key,'g'); // search for all instances
+		let newText = paragraph.replace(re,`<mark>${key}</mark>`);
+		document.getElementById("intro").innerHTML = newText;
+    }else{
+
+    }
+
+    // console.log(paragraph);
+    // paragraph.toLowerCase();
+    // const words=paragraph.split(" ");
+    // for(let i=0;i<words.length;i++){
+    //     if(key.indexOf(words[i])!==-1){
+    //         start.push(i);
+    //     }
+    // }
+    // for(let j=0;j<start.length;j++){
+    //    console.log("index",start[j]);
+    //    console.log(paragraph.substring(start[j],start[j]+ul));
+    // }
+})
+
 console.log('Enter your code here');
