787c1911b402e59d704464d44e28a1268ddc6fea
varunvjr <varun.vjr@gmail.com>
2021-10-28 18:52:00 -0500

    palindrome js done

--- hw3/04-palindrome.js
+++ hw3/04-palindrome.js
@@ -1,4 +1,26 @@
 // Enter your code here
 
+var num=document.getElementById("number");
 
-console.log('Enter your code here');
+num.addEventListener("change",(e)=>{
+    var n1=num.value;
+    console.log("number",n1);
+    var n2=parseInt(n1);
+    var rev=0;
+
+    while(1){
+        if(n2<=0)
+            break;
+        var rem=n2%10;
+        rev=rev*10+rem;
+        n2=Math.floor(n2/10);
+    }
+
+    if(rev==n1){
+        document.getElementById("result").innerText="Yes";
+    }else{
+        document.getElementById("result").innerText="No";
+    }
+    e.preventDefault();
+})
+
