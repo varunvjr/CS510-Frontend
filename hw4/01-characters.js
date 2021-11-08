63ede75fc88d854274814f892d4c8c98ad3acdd9
varunvjr <varun.vjr@gmail.com>
2021-11-07 18:21:29 -0800

    GOT images done

--- hw4/01-characters.js
+++ hw4/01-characters.js
@@ -1,4 +1,25 @@
 // URL to Game of Thrones API to fetch all characters
 let url = 'https://thronesapi.com/api/v2/Characters';
+let characterArray=[];
+fetch(url)
+    .then(res=>res.json())
+    .then(data=>{
+        characterArray=[...characterArray,data];
+        console.log(characterArray[0]);
+        printCharacters(characterArray[0]);
+    })
+    .catch(err=>{console.log("Error",err)})
 
-fetch(url);
+function printCharacters(actor){
+    actor.map(act=>{
+        const div=document.createElement("div");
+        const p=document.createElement("p");
+        p.innerText=act.fullName;
+        div.innerHTML="<img src="+act.imageUrl+" alt="+act.image+"/>"+act.fullName+ "";
+        document.querySelector(".characters").appendChild(div);
+
+        console.log("Name: ",act.imageUrl);
+    })
+}
+
+  
